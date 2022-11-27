c70681f276951c1b8febc66e45cdca955f3d7e6f
Alexandre Delmotte <113446625+Aelex51@users.noreply.github.com>
2022-11-27 17:50:43 +0100

    Add files via upload

---
+++ styleguide/styleguide.php
@@ -0,0 +1,41 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Styleguide</title>
+    <link rel="stylesheet" href="/style.css/">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="mainTitle">Styleguide de Wild Series</h1>
+    <h2>Colors</h2>
+    <section class="colorsContainer">
+        <div class="color1">#9F5540</div>
+        <div class="color2">#EDEDED</div>
+        <div class="color3">#444444</div>
+        <div class="color4">#E5E5E5</div>
+    </section>
+
+    <section>
+        <h2>Typography</h2>
+        <p>Rubik Bold 100 px:</p>
+        <h1 class="RubikBold100">Hello</h1><br /><br />
+        <p>Rubik Bold 38 px:</p>
+        <h1 class="RubikBold38">Contrary to popular belief,<br />
+         Lorem Ipsum is not simply random text.</h1><br /><br />
+         <p>Paragraph</p>
+         <p class="Rubik">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form,<br />
+             by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of<br />
+              Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+    </section>
+</body>
+
+</html>
